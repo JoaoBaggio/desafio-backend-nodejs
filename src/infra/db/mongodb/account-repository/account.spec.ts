b54d7c4421c0b9afcf88b58eea6cc866f78d8dcb
João Baggio <jvictorbaggio@gmail.com>
2021-06-01 17:57:12 -0300

    refactor: move sut creation to a factory helper method

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,39 @@
+import { MongoHelper } from '../helpers/mongo-helpers'
+import { AccountMongoRepository } from './account'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+const mongod = new MongoMemoryServer()
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    const uri = await mongod.getUri()
+    // const port = await mongod.getPort()
+    // const dbPath = await mongod.getDbPath()
+    // const dbName = await mongod.getDbName()
+    // console.log(uri, port, dbPath, dbPath, dbName)
+    await MongoHelper.connect(uri)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    await mongod.stop()
+  })
+
+  it('Should return an account on success', async () => {
+    const sut = makeSut()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.password).toBe('any_password')
+  })
+})
