072066dfcd214d752d5504342f81db8594563fe0
João Baggio <jvictorbaggio@gmail.com>
2021-06-03 17:44:38 -0300

    refactor: mode Adapter folder to main layer

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { makeSignUpController } from '../factories'
-import { adaptRoute } from './adapters'
+import { adaptRoute } from '../adapters'
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
