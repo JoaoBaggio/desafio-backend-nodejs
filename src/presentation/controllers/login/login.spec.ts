aa634cb307d10a458635579f3ba0077bbf1adbfa
João Baggio <jvictorbaggio@gmail.com>
2021-06-03 21:09:50 -0300

    test: enruse LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,16 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helpers'
+import { LoginController } from './login'
+
+describe('Login Controller', () => {
+  it('Should Return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpResquest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpResquest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
