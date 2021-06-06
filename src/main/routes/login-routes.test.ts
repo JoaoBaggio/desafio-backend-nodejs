0a006f9d6ca9ce58f5874cd28b12601b16eb4ad3
João Baggio <jvictorbaggio@gmail.com>
2021-06-06 20:01:52 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,10 +1,12 @@
+import { Collection } from 'mongodb'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
 import app from '../config/app'
+import { hash } from 'bcrypt'
 
 const mongod = new MongoMemoryServer()
-
+let accountCollection: Collection
 describe('Login Routes', () => {
   beforeAll(async () => {
     const uri = await mongod.getUri()
@@ -16,7 +18,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -35,6 +37,34 @@ describe('Login Routes', () => {
           passwordConfirmation: '123'
         })
         .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    it('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Joao',
+        email: 'joao@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'joao@gmail.com',
+          password: '123'
+        })
+        .expect(200)
+    })
+
+    it('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'joao@gmail.com',
+          password: '123'
+        })
+        .expect(401)
     })
   })
 })
