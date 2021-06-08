221771579052b6ebca84a26347f804f22c1e6267
João Baggio <jvictorbaggio@gmail.com>
2021-06-07 23:03:05 -0300

    test: remove useless test

--- src/data/usecases/add-product/db-add-product.ts
+++ src/data/usecases/add-product/db-add-product.ts
@@ -1,7 +1,6 @@
 
 import { ProductModel } from '../../../domain/models/product'
 import { AddProduct, AddProductModel } from '../../../domain/usecases/add-product'
-import { InvalidParamError } from '../../../presentation/errors'
 import { AddProductRepository } from '../../protocols/db/product/add-product-repository'
 
 export class DbAddProduct implements AddProduct {
@@ -10,8 +9,6 @@ export class DbAddProduct implements AddProduct {
   ) {}
 
   async add (product: AddProductModel): Promise<ProductModel | null> {
-    const { factor } = product
-    if (!['A', 'B', 'C'].includes(factor)) return await new Promise((resolve, reject) => reject(new InvalidParamError('factor')))
     return await this.addProductRepository.add(product)
   }
 }
